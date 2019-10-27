8bcc69ca89c077bedc477ae82c384634a33e03fa
ayira-onimisi <onimisi.ayira@mail.utoronto.ca>
2019-10-26 22:57:46 -0400

    Created a simple and empty GUI window
    Created an empty GUI window with a background color of yellow that should serve as the start page of our game, the window is resizeable.

---
+++ start_view.py
@@ -0,0 +1,24 @@
+import pygame
+
+GAME_WINDOW_HEIGHT = 500
+GAME_WINDOW_WIDTH = 600
+
+pygame.display.init()
+
+pygame.display.set_caption("CONNECT FOUR")
+window = pygame.display.set_mode([GAME_WINDOW_WIDTH, GAME_WINDOW_HEIGHT],
+    pygame.RESIZABLE)
+
+yellow = [255, 255, 0]
+exit_game = False
+
+
+if __name__ == "__main__":
+    while not exit_game:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit_game = True
+        window.fill(yellow)
+        pygame.display.update()
+
+    pygame.QUIT()
