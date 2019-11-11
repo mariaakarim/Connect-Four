f65b3c270c301a85eee142739db7d07feb447485
ayira-onimisi <onimisi.ayira@mail.utoronto.ca>
2019-11-10 20:09:35 -0500

    Change name of Chessboard.py and create a board class with functions for adding numbers to 2D array
    Changed chessboard.py to board.py and created a class with functions to add a number called disk to the array stored in <Board> and function to get the next open row given a specific column

---
+++ board.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class Board:
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.cols = columns
+        self.board = np.zeros((rows, columns))
+
+    def place_disk(self, row, col, disk):
+        self.board[self.rows - row -1][col] = disk
+
+    def is_valid_location(self, row, col):
+        return self.board[row][col] == 0
+
+    def print_board(self):
+        print(self.board)
+
+    def get_next_open_row(self, col):
+        for row in range(self.rows):
+            if self.board[self.rows-1-row][col] == 0:
+                return row
+        return None
