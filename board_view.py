c277b1f90b350498b32d4c76b628507cd539bfab
ayira-onimisi <onimisi.ayira@mail.utoronto.ca>
2019-11-10 20:47:56 -0500

    Connects GUI board with 2D array to reflect changes made on 2D array to GUI board and added a few macros for colors
    Imported board class into board_view.py and made a game_board that's an instance of Board class to use as the official board of the game. Added red and purple macros to store those colors in RGB format.

--- board_view.py
+++ board_view.py
@@ -1,4 +1,5 @@
 import pygame
+from board import *
 
 NUM_COLS = 7
 NUM_ROWS = 6
@@ -10,6 +11,8 @@ RADIUS = 35
 INPUT_BAR = [0, 0, NUM_COLS*BLOCK, RADIUS*2]
 YELLOW = [255, 255, 0]
 WHITE = [255, 255, 255]
+RED = [255, 0, 0]
+PURPLE = [255, 0, 255]
 
 pygame.display.init()
 
@@ -18,19 +21,41 @@ window = pygame.display.set_mode([WIDTH, HEIGHT],
     pygame.RESIZABLE)
 
 exit_game = False
+game_board = Board(NUM_ROWS, NUM_COLS)
+
+def draw_player1_disk(row, col):
+    circle = [(col*BLOCK) + BLOCK//2, (row*BLOCK)+ BLOCK + BLOCK//2]
+    pygame.draw.circle(window, RED, circle, RADIUS)
+
+def draw_player2_disk(row, col):
+    circle = [(col*BLOCK) + BLOCK//2, (row*BLOCK)+ BLOCK + BLOCK//2]
+    pygame.draw.circle(window, YELLOW, circle, RADIUS)
+
+def draw_empty_disk(row, col):
+    circle = [(col*BLOCK) + BLOCK//2, (row*BLOCK)+ BLOCK + BLOCK//2]
+    pygame.draw.circle(window, WHITE, circle, RADIUS)
+
+def display_board():
+    for col in range(NUM_COLS):
+        for row in range(NUM_ROWS):
+            if game_board.board[row, col] == 1:
+                draw_player1_disk(row, col)
+            elif game_board.board[row, col] == 2:
+                draw_player2_disk(row, col)
+            else:
+                draw_empty_disk(row, col)
+
 
 if __name__ == "__main__":
     while not exit_game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit_game = True
-        window.fill(YELLOW)
+        window.fill(PURPLE)
         pygame.draw.rect(window, WHITE, INPUT_BAR, 0)
         #fills board with white circles to represent empty slots in the game
-        for col in range(NUM_COLS):
-            for row in range(NUM_ROWS):
-                circle = [(col*BLOCK) + BLOCK//2, (row*BLOCK)+ BLOCK + BLOCK//2]
-                pygame.draw.circle(window, WHITE, circle, RADIUS)
+        display_board()
+
         pygame.display.update()
 
     pygame.quit()
