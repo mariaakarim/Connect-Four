f84e3946dc9a18d4d36dfb90f3d5ebccdf5cb2d2
simb2 <1simranbilkhu@gmail.com>
2019-11-15 20:57:53 -0500

    Add Label functionality

--- button.py
+++ button.py
@@ -8,14 +8,20 @@ class PG_Button:
         self.base = base
         self.height = height
         self.txt = txt
+
     def draw(self, window):
-        pygame.draw.rect(window, self.col, self.x_coor, self.y_coor, self.base, self.height)
+        pygame.draw.rect(window, self.col, (self.x_coor, self.y_coor, self.base, self.height))
+        text = pygame.font.SysFont("comicsans", 30)
+        label = text.render(self.txt, 1, (0,0,0))
+        window.blit(label, (self.x_coor+(self.base//2)//2, self.y_coor+(self.height//2)//2))
 
     def mouse_over(self, position):
         if position[0] > self.x_coor and position[0] < self.x_coor + self.base:
-            if position[1] > self.y_coor and position[1] < self.y + self.height:
+            if position[1] > self.y_coor and position[1] < self.y_coor + self.height:
                 return True
         return False
+
+
 
 
 
