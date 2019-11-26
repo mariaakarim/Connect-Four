2f57a8d81de280b709797cdcd8347f9d9d3a1bab
simb2 <1simranbilkhu@gmail.com>
2019-11-26 08:50:40 -0500

    add win clauses for vertical and horizontal

--- board.py
+++ board.py
@@ -20,4 +20,57 @@ class Board:
         for row in range(self.rows):
             if self.board[self.rows-1-row][col] == 0:
                 return row
-        return None
+
+    def check_winner(self, player, row, col):
+        if self.check_horizontal(row, col,  player) or self.check_vertical(row, col, player):
+            return True
+        return False
+
+    def check_horizontal(self, row, col, player):
+        pos = self.rows - row - 1
+        total = 0
+        #check left
+        while pos > 0:
+            if (self.board[pos][col] == player):
+                total += 1
+            else:
+                break
+            pos -= 1
+
+
+        pos = self.rows - row
+
+        while pos < self.rows:
+            if self.board[pos][col] == player:
+                total += 1
+            else:
+                break
+            pos += 1
+
+        if total == 4:
+            return True
+        return False
+
+    def check_vertical(self, row, col, player):
+        c = col
+        r = self.rows - row - 1
+        total = 0
+        while c > 0:
+            if self.board[r][c] == player:
+                total += 1
+            else:
+                break
+            c -= 1
+        c = col + 1
+        while c < self.rows:
+            if self.board[r][c] == player:
+                total += 1
+            else:
+                break
+            c += 1
+        if total == 4:
+            return True
+        return False
+
+
+
