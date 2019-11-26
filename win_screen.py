05b0f459e723b0ac36daf7568f7bbd50ca1ebc99
simb2 <1simranbilkhu@gmail.com>
2019-11-26 08:53:52 -0500

    add win screen

---
+++ win_screen.py
@@ -0,0 +1,20 @@
+import pygame
+
+pygame.init()
+def display():
+    window = pygame.display.set_mode((300, 300))
+    pygame.display.set_caption("Winner!")
+    pygame.display.update()
+    on = True
+    while on:
+        window.fill((0, 0, 0))
+        text = pygame.font.SysFont("comicsans", 30)
+        label = text.render("You win!", True, (0, 128, 0))
+        window.blit(label, (110, 150))
+
+        for event in pygame.event.get():
+            if(event.type == pygame.QUIT):
+                on = False
+
+        pygame.display.flip()
+    pygame.quit()
