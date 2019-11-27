e6a2cfb1a3500a393103f040911effe0ee00a3ac
Maria A. Karim 💕 <maria@Marias-MacBook-Pro.local>
2019-11-27 17:42:43 -0500

    added doc string and code

--- Move.py
+++ Move.py
@@ -1,43 +1,43 @@
 class Move:
-    
-    def __init__(row, col):
+
+    def __init__(self, row, col):
         """ Initializes a new move with given row and column.
         """
         # === Representation Invariants ===
         # - row >= 1 and row <= 6
         # - col >= 1 and col <= 7
-        
+
         self.row = row
         self.col = col
 
-    def getRowNum():
+    def getRowNum(self):
         """ Returns the row number.
         """
         return self.row
 
-    def getColNum():
+    def getColNum(self):
         """ Returns the column number.
         """
         return self.col
 
-    def is_empty(row, col):
+    def is_empty(self, row, col):
         """ Return True iff the position at row and col is an empty slot."
         """
-        
+
         if self.chessBoard[row][col] == "EMPTY":
             return True
         else:
             return False
-    
-    def checkValidMove(row, col):
+
+    def checkValidMove(self, row, col):
         """ Return True iff the given move at row and col is valid.
         """
 
         if self.is_empty(row,col):
-            
+
             return True
 
         return False
-        
 
-    
+
+
--- board.py
+++ board.py
@@ -1,44 +1,71 @@
 import numpy as np
 
+
 class Board:
+
     def __init__(self, rows, columns):
+        """ Initialize a the board with the given rows and columns
+
+        """
         self.rows = rows
         self.cols = columns
         self.board = np.zeros((rows, columns))
 
     def place_disk(self, row, col, disk):
-        if(self.is_valid_location(row, col)):
+        """  Place a disk on the board at the given row and col.
+        """
+
+        if self.is_valid_location(row, col):
             self.board[self.rows - row -1][col] = disk
 
     def is_valid_location(self, row, col):
+        """ Return True iff the position at the given row and col
+        the board is 0.
+        """
+
         return self.board[self.rows - row -1][col] == 0
 
     def print_board(self):
+        """Print the Connect Four Board
+        """
+
         print(self.board)
 
     def get_next_open_row(self, col):
+        """ Return the next open row in the given column
+        """
+
         for row in range(self.rows):
             if self.board[self.rows-1-row][col] == 0:
                 return row
 
     def check_winner(self, player, row, col):
-        if self.check_horizontal(row, col,  player) or self.check_vertical(row, col, player):
+        """ Return True iff the player is a winner at the given
+        row and column and has 4 consecutive disks.
+        """
+
+        if self.check_horizontal(row, col,  player) or \
+                self.check_vertical(row, col, player) or self.check_diagonal(row, col, player):
             return True
         return False
 
     def check_horizontal(self, row, col, player):
+        """ Return True iff the player has 4 consecutive disks
+        in the horizontal direction.
+        """
+
         pos = self.rows - row - 1
         total = 0
         #check left
         while pos > 0:
-            if (self.board[pos][col] == player):
+            if self.board[pos][col] == player:
                 total += 1
             else:
                 break
             pos -= 1
 
-
         pos = self.rows - row
+
         while pos < self.rows:
             if self.board[pos][col] == player:
                 total += 1
@@ -51,6 +78,10 @@ class Board:
         return False
 
     def check_vertical(self, row, col, player):
+        """ Return True iff the player has 4 consecutive disks
+        in the vertical direction.
+        """
+
         c = col
         r = self.rows - row - 1
         total = 0
@@ -71,6 +102,37 @@ class Board:
         if total == 4:
             return True
         return False
+
+    def check_diagonal(self, row, col, player):
+        """ Return True iff the player has 4 consecutive disks
+        in the diagonal direction.
+        """
+
+        curr_row = self.rows - row - 1
+        curr_col = col
+        disk_total = 0
+
+        while curr_row > 0:
+            while curr_col > 0:
+                if self.board[curr_row][curr_col]==player:
+                    disk_total+=1
+                curr_col -= 1
+            curr_row -= 1
+
+        curr_col = col + 1
+        curr_row = self.rows - row
+
+        while curr_row < self.rows:
+            while curr_col < self.cols:
+                if self.board[curr_row][curr_col] == player:
+                    disk_total += 1
+                curr_col += 1
+            curr_row += 1
+        if disk_total == 4:
+            return True
+        return False
+
+
 
 
 
