6b1bd1954d6fa825335fc3a5730633c58538c1ed
ayira-onimisi <onimisi.ayira@mail.utoronto.ca>
2019-12-03 18:35:23 -0500

    Delete unused files
    We ended up not using these files so out they go.

--- Player.py
+++
@@ -1,9 +0,0 @@
-# simple class for storing the name and colour choice of each
-# player
-
-class Player:
-    def __init__(self, name, colour):
-        self.name = name
-        self.colour = colour
-        self.is_turn = None
-
