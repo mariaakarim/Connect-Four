386215e2e1a1a5611ece92a2489d75df8ce58607
ayira-onimisi <onimisi.ayira@mail.utoronto.ca>
2019-12-03 17:46:53 -0500

    Implement checking diagonals for wins
    Correctly implements the win cases when discs are arranged diagonally

--- board.py
+++ board.py
@@ -45,7 +45,9 @@ class Board:
         """
 
         if self.check_horizontal(row, col,  player) or \
-                self.check_vertical(row, col, player) or self.check_diagonal(row, col, player):
+                self.check_vertical(row, col, player) or \
+                self.check_left_diagonal(row, col, player) or \
+                self.check_right_diagonal(row, col, player):
             return True
         return False
 
@@ -103,36 +105,42 @@ class Board:
             return True
         return False
 
-    def check_diagonal(self, row, col, player):
-        """ Return True iff the player has 4 consecutive disks
-        in the diagonal direction.
+    def check_left_diagonal(self, row, col, player):
+        """ Return True iff the player has 4 consecutive disks in the negative
+        left diagonal direction.
         """
+        consec_num = 1
+        if col > 3 and row < 3:
+            return False
 
-        curr_row = self.rows - row - 1
-        curr_col = col
-        disk_total = 0
+        for i in range(row, row - 4, -1):
+            sel_row = self.rows - row - 1
+            if consec_num == 4:
+                return True
+            if sel_row + 1 < self.rows:
+                if self.board[sel_row + 1][col-1] == player:
+                    consec_num += 1
+            row -= 1
+            col -= 1
 
-        while curr_row > 0:
-            while curr_col > 0:
-                if self.board[curr_row][curr_col]==player:
-                    disk_total+=1
-                curr_col -= 1
-            curr_row -= 1
-
-        curr_col = col + 1
-        curr_row = self.rows - row
-
-        while curr_row < self.rows:
-            while curr_col < self.cols:
-                if self.board[curr_row][curr_col] == player:
-                    disk_total += 1
-                curr_col += 1
-            curr_row += 1
-        if disk_total == 4:
-            return True
         return False
 
+    def check_right_diagonal(self, row, col, player):
+        """ Return True iff the player has 4 consecutive disks in the negative
+        right diagonal direction.
+        """
+        consec_num = 1
+        if col < 3 and row < 3:
+            return False
 
+        for i in range(row, row - 4, -1):
+            sel_row = self.rows - row - 1
+            if consec_num == 4:
+                return True
+            if sel_row + 1 < self.rows:
+                if self.board[sel_row + 1][col+1] == player:
+                    consec_num += 1
+            row -= 1
+            col += 1
 
-
-
+        return False
